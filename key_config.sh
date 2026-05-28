a351e13690d5c45a18575fc09ee14efdccf91a70
ajayaispoc-oss <ajay.ai.spoc@gmail.com>
2026-05-28 19:43:55 +0000

    push3

---
+++ key_config.sh
@@ -0,0 +1,7 @@
+export DOCUMENT_INTELLIGENCE_ENDPOINT="https://agentic-doc-intelli.cognitiveservices.azure.com/"
+export DOCUMENT_INTELLIGENCE_KEY="key generated after document intelligenece configured"
+export AZURE_OPENAI_ENDPOINT="https://azure-endp-resource.services.ai.azure.com"
+export AZURE_OPENAI_API_KEY="key generated after openaideployment"
+export AZURE_OPENAI_DEPLOYMENT="gpt-5-mini"
+export SourceBlobConnectionString="DefaultEndpointsProtocol=https;AccountName=azsaname;AccountKey=key "
+
